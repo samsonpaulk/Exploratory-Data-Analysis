350969334385eae265f81eb02a58e3f207f0c35b
samsonpaulk <samsonpaulk@users.noreply.github.com>
2014-07-13 19:25:52 -0400

    Update README.R

--- README.R
+++ README.R
@@ -7,6 +7,7 @@
 <html class="   ">
   <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# object: http://ogp.me/ns/object# article: http://ogp.me/ns/article# profile: http://ogp.me/ns/profile#">
     <meta charset='utf-8'>
+    
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     
     
